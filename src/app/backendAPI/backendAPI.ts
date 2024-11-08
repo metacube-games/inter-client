82d0a9fcdd4ae341f28fb66188bc542912a01895
KamyarTaher <61198701+KamyarTaher@users.noreply.github.com>
2024-11-08 23:12:54 +0100

    s

--- src/app/backendAPI/backendAPI.ts
+++ src/app/backendAPI/backendAPI.ts
@@ -20,7 +20,7 @@ const createApi = () => {
 
 let api = createApi();
 function setAccessTokenCookie(token: string) {
-  document.cookie = `refreshToken=${token}; path=/; HttpOnly; Secure; SameSite=None; Max-Age=86400;`;
+  document.cookie = `refreshToken=${token}; path=/; Secure; SameSite=None; Max-Age=86400;`;
 }
 
 // document.cookie = `refreshToken=${token}; Path=/; Max-Age=86400; HttpOnly; Secure; SameSite=None`;
